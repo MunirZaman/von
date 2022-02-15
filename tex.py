b17195c9171a75e21d3648a64c6fdb34a49510fd
MunirZaman <bijli.begum.bd@gmail.com>
2022-02-15 11:58:45 +0600

    updated tex.py - added makeTeXAll

--- tex.py
+++ tex.py
@@ -3,6 +3,7 @@ import subprocess
 import pathlib
 
 from . import model
+from . import view
 
 LATEXMK = ['latexmk', '-pdflatex']
 LATEXMK_CLEAN = ['latexmk', '-c']
@@ -23,11 +24,11 @@ TEX = \
 """
 
 
-def compileTeX(path, opts = []):
-    subprocess.call([*LATEXMK, *opts, path])
+def compileTeX(path, opts = [], stdout = subprocess.STDOUT):
+    subrun = subprocess.run([*LATEXMK, *opts, path], stdout = stdout)
     parent = pathlib.PurePath(path).parent
     filename = pathlib.PurePath(path).name.replace('.tex', '.pdf')
-    os.system(filename)
+    #os.system(filename)
 
 def getTeXsrc(obj):
     if isinstance(obj, model.Problem):
@@ -52,10 +53,13 @@ def makeTeX(obj):
         return None
 
     if isinstance(obj, model.Problem):
-        fn =  obj.label.replace(" ",  "-").replace("/", "-") + ".tex"
+        #fn =  obj.label.replace(" ",  "-").replace("/", "-") + ".tex"
+        fn = pathlib.PurePath(model.completePath(obj.path)).name
+        # should i use label as file name?
     elif isinstance(obj, model.IndexEntry):
         obj = obj.full
-        fn =  obj.label.replace(" ",  "-").replace("/", "-") + ".tex"
+        #fn =  obj.label.replace(" ",  "-").replace("/", "-") + ".tex"
+        fn = pathlib.PurePath(model.completePath(obj.path)).name
 
     try:
         with open(fn, 'x') as f:
@@ -71,3 +75,43 @@ def cleanTeX():
     subprocess.call([*LATEXMK_CLEAN])
 
     
+def makeTeXAll(folder, compile=True):
+    cwd = os.getcwd()
+    if compile:
+        view.warn("This might take a long time.")
+
+    for entry in model.VonIndex().values():
+        path = model.completePath(entry.path)
+        purepath = pathlib.PurePath(path)
+        parent = purepath.parent
+        fname = purepath.name
+
+        #create directory
+        short_parent = model.shortenPath(parent)
+        new_parent = os.path.join(folder, short_parent)
+        try:
+            os.makedirs(new_parent)
+        except FileExistsError:
+            pass
+        
+        # write problems to files
+        new_path = os.path.join(folder, short_parent, fname)
+        texsrc = getTeXsrc(entry)
+
+        try:
+            with open(new_path, 'x') as file:
+                file.write(texsrc)
+        except FileExistsError:
+            with open(new_path, 'w') as file:
+                file.write(texsrc)            
+
+        if compile:
+            os.chdir(new_parent)
+            os.system(' '.join([*LATEXMK, fname]))
+            os.system(' '.join(LATEXMK_CLEAN))
+            os.chdir(cwd)
+            view.log(f"Successfully compiled {new_path}")
+
+    os.chdir(cwd)
+
+
