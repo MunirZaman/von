fbe4949cb1874232f53c34ebddea174de0198ff0
MunirZaman <bijli.begum.bd@gmail.com>
2022-02-15 14:22:39 +0600

    updated controller.tex.py * added args -l and -a

--- controller/tex.py
+++ controller/tex.py
@@ -2,13 +2,17 @@ from cmd2 import Cmd2ArgumentParser, with_argparser
 from .. import view, model, tex
 
 latex = Cmd2ArgumentParser(prog="latex", usage="latex")
-latex.add_argument('label')
+latex.add_argument('-l', nargs=1, default=[], type=str, dest="label")
+latex.add_argument('-a', action="store_true")
 
 @with_argparser(latex)
 def main(self, opts):
-    entry = model.VonIndex()[opts.label]
-    fn = tex.makeTeX(entry)
-    print(fn)
-    tex.compileTeX(fn)
-    tex.cleanTeX()
+    if opts.a:
+        tex.makeTeXAll()
+
+    elif len(opts.label) == 1:
+        entry = model.VonIndex()[opts.label[0]]
+        fn = tex.makeTeX(entry)
+        tex.compileTeX(fn)
+        tex.cleanTeX()
 
